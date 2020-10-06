7f93fe0ee29206c6595edf2965ffe0c749643892
ste <foalestephen@gmail.com>
2020-10-06 19:53:51 +0100

    6

--- scroll.js
+++ scroll.js
@@ -26,10 +26,39 @@ function updatescroll() {
 }
 window.requestAnimationFrame(updatescroll);
 
-//var item = document.getElementsByTagName('body')[0];
-//
-//window.addEventListener('wheel', function(e) {
-//
-//  if (e.deltaY > 0) item.scrollLeft += 100;
-//  else item.scrollLeft -= 100;
-//});
+
+//wheel
+var item = document.getElementsByTagName('body')[0];
+
+window.addEventListener('wheel', function(e) {
+
+  if (e.deltaY > 0) item.scrollLeft += 100;
+  else item.scrollLeft -= 100;
+});
+
+//links
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
